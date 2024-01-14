3c859353f8b4a64b959a72d45a43a0aaa0223b65
SamirAhma <ahmaddin1000@gmail.com>
2024-01-14 21:51:28 +0800

    add variable

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
+var name = "Ahmaddin";
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+  return (
+    <>
+      <Text style={styles.textStyle}>Getting started with React Native!</Text>
+      <Text style={styles.nameStyle}>My name is {name} </Text>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 60,
+    fontSize: 45,
+  },
+  nameStyle: {
+    fontSize: 20,
   },
 });
 
